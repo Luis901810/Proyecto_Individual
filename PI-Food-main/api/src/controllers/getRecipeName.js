bdf0cb2a9d499d56dca76c389cd952f0ef9674af
unknown <luis901810@gmail.com>
2023-08-21 12:07:27 -0500

    proyecto

--- PI-Food-main/api/src/controllers/getRecipeName.js
+++ PI-Food-main/api/src/controllers/getRecipeName.js
@@ -7,7 +7,9 @@ const getRecipesName = async(req, res) =>{
     try {
      let { name } = req.query
         if(!name) throw new Error("No hay recetas con ese nombre")
-        const { data } = await axios(`${URLAPI}/complexSearch/?apiKey=${YOUR_API_KEY}&number=100`)
+
+
+        const { data } = await axios(`${URLAPI}/complexSearch/?apiKey=${YOUR_API_KEY}&number=100&addRecipeInformation=true`)
         const matchingRecipes= data.results.filter(Recipe => Recipe.title.toLowerCase().includes(name.toLowerCase()))
         
         return res.status(200).json(matchingRecipes)
@@ -16,7 +18,8 @@ const getRecipesName = async(req, res) =>{
 
         
     } catch (error) {
-        return res.status(404).json({error: error.message})
+        return res.status(400).json({error: error.message})
+       
         
     }
 }
